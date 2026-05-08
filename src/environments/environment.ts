4f820da766925a1f133175c603077506ac46aeb1
Llummo <167897805+Llummo@users.noreply.github.com>
2026-05-08 08:02:24 -0500

    chore: add production api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  equipmentApiProvider: "http://localhost:3000",
-  equipmentEnpoints: "/equipments",
-  iotEndpoint: "/iot_devices"
+  equipmentApiProvider: 'spot-track-apiv1.azurewebsites.net',
+  equipmentEnpoints: '/equipments',
+  iotEndpoint: '/iot_devices',
 };
