19491172a67e88afc79bbb9b9d49264ce51521d3
jcus1510 <167936372+jcus1510@users.noreply.github.com>
2026-05-10 23:34:18 -0500

    feat:language switcher

--- src/app/profiles/presentation/views/profile-view.ts
+++ src/app/profiles/presentation/views/profile-view.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { LanguageSwitcher } from '../../../shared/presentation/components/language-switcher/language-switcher';
 
 @Component({
   selector: 'app-profile-view',
   standalone: true,
   templateUrl: './profile-view.html',
   styleUrl: './profile-view.scss',
+  imports: [LanguageSwitcher],
 })
 export class ProfileView {}
