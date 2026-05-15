063470ffde41a7f4003fb160cbe3045fe0063ad0
ORION-tech-c <asfernandezl.peru@gmail.com>
2026-05-14 23:00:58 -0500

    fixed api errors

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,23 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { fileURLToPath, URL } from 'node:url';
+import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite';
+import { resolve } from 'path';
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VueI18nPlugin({
+      include: resolve(__dirname, './src/i18n/**'),
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  build: {
+    target: 'es2022',
+    sourcemap: true
+  }
 });
