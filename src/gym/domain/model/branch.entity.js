2979517de17ae841f51ba1f94eed63e3eba13bb2
ORION-tech-c <asfernandezl.peru@gmail.com>
2026-06-20 22:16:58 -0500

    feat(gym): scaffold gym.entity, branch.entity, zone.entity

---
+++ src/gym/domain/model/branch.entity.js
@@ -0,0 +1,9 @@
+export class Branch {
+  constructor({ id, name, street, district, city }) {
+    this.id       = id;
+    this.name     = name;
+    this.street   = street;
+    this.district = district;
+    this.city     = city;
+  }
+}
