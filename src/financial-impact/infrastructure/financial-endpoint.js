04f067022ec1e859303a998d9caf4aa07080f713
ORION-tech-c <asfernandezl.peru@gmail.com>
2026-06-21 02:13:33 -0500

    refactor(analytics): remove financial-impact domain and infrastructure files

--- src/financial-impact/infrastructure/financial-endpoint.js
+++
@@ -1,8 +0,0 @@
-import { BaseEndpoint } from '@/shared/infrastructure/base-endpoint.js';
-import { FinancialAssembler } from './financial-assembler.js';
-
-export class FinancialEndpoint extends BaseEndpoint {
-  constructor(api) {
-    super(api, 'equipment_usage_stats', new FinancialAssembler());
-  }
-}
