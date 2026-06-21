d85d4e1dfe27493d384431146568742a59f1cddf
ORION-tech-c <asfernandezl.peru@gmail.com>
2026-06-21 02:15:17 -0500

    feat(analytics): add RoiProjectionApi with create, updateProjectedEarnings, generate

---
+++ src/analytics/infrastructure/roi-projection-api.js
@@ -0,0 +1,21 @@
+import { BaseApi } from '@/shared/infrastructure/base-api.js';
+import { RoiProjectionAssembler } from './roi-projection-assembler.js';
+
+const asm = new RoiProjectionAssembler();
+
+export class RoiProjectionApi extends BaseApi {
+  async create(projectedDowntimeCost) {
+    const { data } = await this.http.post('roi-projections', { projectedDowntimeCost });
+    return asm.toEntityFromResource(data);
+  }
+
+  async updateProjectedEarnings(roiProjectionId, projectedEarnings) {
+    const { data } = await this.http.post('roi-projections/projected-earnings', { roiProjectionId, projectedEarnings });
+    return asm.toEntityFromResource(data);
+  }
+
+  async generate(roiProjectionId) {
+    const { data } = await this.http.post('roi-projections/generate', { roiProjectionId });
+    return asm.toEntityFromResource(data);
+  }
+}
