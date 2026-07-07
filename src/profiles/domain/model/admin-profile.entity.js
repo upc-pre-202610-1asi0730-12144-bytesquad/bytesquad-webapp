17c4464d15977903c516109fa52afad0f3771c32
ORION-tech-c <asfernandezl.peru@gmail.com>
2026-07-07 00:16:55 -0500

    fix(profiles): split fullName into firstName/lastName in AdminProfile entity, add fullName getter

--- src/profiles/domain/model/admin-profile.entity.js
+++ src/profiles/domain/model/admin-profile.entity.js
@@ -1,10 +1,13 @@
 export class AdminProfile {
-  constructor({ id, userId, email, fullName, phoneNumber, dni }) {
-    this.id = id;
-    this.userId = userId;
-    this.email = email;
-    this.fullName = fullName;
+  constructor({ id, userId, email, firstName, lastName, phoneNumber, dni }) {
+    this.id          = id;
+    this.userId      = userId;
+    this.email       = email;
+    this.firstName   = firstName;
+    this.lastName    = lastName;
     this.phoneNumber = phoneNumber;
-    this.dni = dni;
+    this.dni         = dni;
   }
+
+  get fullName() { return `${this.firstName ?? ''} ${this.lastName ?? ''}`.trim(); }
 }
