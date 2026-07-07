8c11ef55ea8a50c860f6dafe3f5d68bed1d939dd
ORION-tech-c <asfernandezl.peru@gmail.com>
2026-07-07 10:20:55 -0500

    feat(maintenance): add MaintenancePriority and MaintenanceType enums, priority/type fields to entity

--- src/maintenance/domain/model/maintenance.entity.js
+++ src/maintenance/domain/model/maintenance.entity.js
@@ -1,3 +1,14 @@
+export const MaintenancePriority = Object.freeze({
+  Low:    'Low',
+  Medium: 'Medium',
+  High:   'High',
+});
+
+export const MaintenanceType = Object.freeze({
+  Corrective: 'Corrective',
+  Preventive: 'Preventive',
+});
+
 export const MaintenanceStatus = Object.freeze({
   Requested: 'Requested',
   InProgress: 'InProgress',
@@ -6,11 +17,13 @@ export const MaintenanceStatus = Object.freeze({
 });
 
 export class Maintenance {
-  constructor({ id, equipmentId, requestedByAdminId, reason, status }) {
+  constructor({ id, equipmentId, requestedByAdminId, reason, status, priority, type }) {
     this.id = id;
     this.equipmentId = equipmentId;
     this.requestedByAdminId = requestedByAdminId;
     this.reason = reason;
     this.status = status;
+    this.priority = priority;
+    this.type = type;
   }
 }
